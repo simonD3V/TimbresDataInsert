0fba2f0873b43a7af2e297ff3027d3f936c1273a
simonD3V <72438458+simonD3V@users.noreply.github.com>
2021-02-16 14:35:24 +0100

    Update timbres_data_insert.py

--- timbres_data_insert.py
+++ timbres_data_insert.py
@@ -3,7 +3,17 @@
 import yaml
 import os
 import json
+import uuid
 
+# ---------FUNCTIONS------------------
+
+def uuid_generation(n) :
+    list = []
+    for i in range (int(n)) :
+        u = uuid.uuid4()
+        list.append(str(u))
+    return list
+    
 
 # ----------MAIN----------------------
 
@@ -13,12 +23,20 @@ if __name__ == "__main__":
     pwd = '?Tr;_Q$D2W4#2!aG'
     auth_json = '{ "email" : "%s", "password" : "%s"}' % (email, pwd)
 
-    url = 'http://bases-iremus.huma-num.fr/timbres/auth/login'
+    url = 'http://bases-iremus.huma-num.fr/timbres'
 
     # curl -X POST -H 'Content-Type: application/json' --data '{'email': 'thomas.bottini@cnrs.fr', 'password' : '?Tr;_Q$D2W4#2!aG'}' http://bases-iremus.huma-num.fr/timbres/auth/login
     token_request = os.popen(
-        "curl -X POST -H 'Content-Type: application/json' --data '%s' %s" % (auth_json, url)).read()
-    token = str(json.loads(token_request)['data']['access_token'])
+        "curl -X POST -H 'Content-Type: application/json' --data '%s' %s/auth/login" % (auth_json, url)).read()
+    token = str(json.loads(token_request)['data']['refresh_token'])
     # token captured
     print('\ntoken : ' + token)
-    
+
+    # insertion test theme
+
+    uuid_test = uuid.uuid4()
+    theme = '{"id" : "%s", "theme" : "antoine", "type" : "patronyme", "textes_publies" : null }' % (uuid_test)
+    insertion_theme = os.popen("curl -X POST -H 'Content-Type: application/json' --data '%s' %s/items/themes? access_token=%s" % (theme, url, token))
+
+
+
